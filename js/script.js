d27da16e7f6ec580e8212c9845f512a0a6466370
Marco Tessitore <marcotexcode@gmail.com>
2021-06-21 14:22:30 +0200

    esercizio + bonus

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+
+const app = new Vue({
+
+    el: '#root',
+
+    data: {
+
+        message: 'Hello World',
+
+        image1: '1.jpeg',
+        image2: '2.jpeg',
+        image3: '3.jpeg',
+        image4: '4.jpeg',
+
+    }
+
+});
